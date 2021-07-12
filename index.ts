671993a162c0676ef47e8e962be6226da4110aad
Alexander Liu <hey@alexanderliu.com>
2021-07-12 16:33:00 -0400

    feat: request course information from websoc and alert subscribed users

--- index.ts
+++ index.ts
@@ -5,6 +5,9 @@ import * as path from "path";
 import { Db, MongoClient, MongoClientOptions } from "mongodb";
 import { RelayClient, RelayConsumer } from "@signalwire/node";
 import { IMessage } from "@signalwire/node/dist/common/src/util/interfaces";
+import FormData from "form-data";
+import axios from "axios";
+import { parseStringPromise } from "xml2js";
 
 const commands = new Map();
 
@@ -92,4 +95,91 @@ const consumer = new RelayConsumer({
 mongoClient.connect().then(async (mongoClient) => {
   mongoDb = mongoClient.db(process.env.MONGO_DB);
   await consumer.run();
+
+  setInterval(async () => {
+    const allClassesArray = await mongoDb
+      .collection("classes")
+      .aggregate([
+        {
+          $project: {
+            _id: 0,
+            code: 1,
+            status: 1,
+          },
+        },
+      ])
+      .toArray();
+    const statuses = allClassesArray.reduce((accumulator, currentValue) => {
+      accumulator[currentValue.code] = currentValue.status;
+      return accumulator;
+    }, {});
+    const codes = allClassesArray.map((classCode) => classCode.code).join(",");
+
+    let form = new FormData();
+    await form.append("YearTerm", "2021-92");
+    await form.append("CourseCodes", codes);
+    await form.append("Submit", "Display XML Results");
+
+    const res = await axios({
+      method: "post",
+      url: "https://www.reg.uci.edu/perl/WebSoc",
+      headers: {
+        ...form.getHeaders(),
+        "Content-Length": form.getLengthSync(),
+      },
+      data: form,
+    });
+    if (res.status === 200) {
+      const course_list = (await parseStringPromise(res.data)).websoc_results
+        .course_list;
+      for (const course of course_list) {
+        for (const school of course.school) {
+          for (const department of school.department) {
+            for (const course of department.course) {
+              for (const section of course.section) {
+                let courseCode = section.course_code[0];
+                let secStatus = section.sec_status[0];
+                await mongoDb.collection("classes").updateOne(
+                  { code: courseCode },
+                  {
+                    $set: {
+                      code: courseCode,
+                      status: secStatus,
+                    },
+                  },
+                  { upsert: true }
+                );
+                if (statuses[courseCode] !== secStatus) {
+                  const allNumbersArray = await mongoDb
+                    .collection("users")
+                    .aggregate([
+                      {
+                        $match: {
+                          classes: { $in: [courseCode] },
+                        },
+                      },
+                      {
+                        $project: {
+                          _id: 0,
+                          number: 1,
+                        },
+                      },
+                    ])
+                    .toArray();
+                  allNumbersArray.forEach((number) => {
+                    client.messaging.send({
+                      context: "notification",
+                      from: "+16617644377",
+                      to: number.number,
+                      body: `${courseCode}'s status changed from "${statuses[courseCode]}" to "${secStatus}"\n\nReply "STOP" to stop receiving all messages.`,
+                    });
+                  });
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }, 20000);
 });
